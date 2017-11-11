195fb804c2dd6a116eed9f1342cf1e4535fca587
Luis Alfredo León Villapún <leonvillapun@gmail.com>
2017-11-11 16:10:51 -0600

    Se agregó la práctica 7 con sus datasets, aún no se termina

---
+++ P7/p7.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 7/11/2017
+
+@author: leonvillapun
+"""
+
+#Data preprocessing template
+
+#Importing the libraries
+
+import numpy as np #Libreria de matematicas
+import matplotlib.pyplot as plt #Libreria de graficas
+import pandas as pd #Libreria para importar set de datos y administrarlos
+ 
+#Import data sets
+ 
+t_dataset = pd.read_csv('iris.csv')
+
+
+ 
